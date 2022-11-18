57e471e77115a8cb0b88f675ab466712a9ca12fc
amoryaubert <amory.aubert@gmail.com>
2022-11-18 14:16:55 +0100

    try post axios

--- server/src/api/v1/todo.js
+++ server/src/api/v1/todo.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const sqlite3 = require("sqlite3");
+const axios = require("axios");
 const router = express.Router();
 
 const db = new sqlite3.Database("./src/db/todo.db", (err) => {
 	if (err) throw err;
 });
 
-let items = [];
+let items;
 db.get("SELECT * FROM todo", (err, data) => {
 	if (err) throw err;
-	items.push(data);
+	items = data;
 	console.log(data);
 });
 
@@ -18,5 +19,20 @@ router.get("/", (req, res) => {
 		items,
 	});
 });
+
+// const data = {
+// 	name: "John Doe",
+// 	job: "Content Writer",
+// };
+
+// axios
+// 	.post("http://localhost:4200/api/v1/todo", data)
+// 	.then((res) => {
+// 		//console.log(`Status: ${res.status}`);
+// 		console.log("Body: ", res.data);
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 	});
 
 module.exports = router;
