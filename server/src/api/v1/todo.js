c217efa13359a06915086862d62e5e3718b33cf2
amoryaubert <amory.aubert@gmail.com>
2022-11-18 13:33:46 +0100

    db insert + select

--- server/src/api/v1/todo.js
+++ server/src/api/v1/todo.js
@@ -1,19 +1,18 @@
 const express = require("express");
-
+const sqlite3 = require("sqlite3");
 const router = express.Router();
 
-const items = [
-	{
-		uuid: "00000000-0000-0000-0000-000000000000",
-		label: "Faire mon repository",
-		done: false,
-	},
-	{
-		uuid: "00000000-0000-0000-0000-000000000000",
-		label: "Faire mon repository",
-		done: false,
-	},
-];
+const db = new sqlite3.Database("./src/db/todo.db", (err) => {
+	if (err) throw err;
+});
+
+let items = [];
+db.get("SELECT * FROM todo", (err, data) => {
+	if (err) throw err;
+	items.push(data);
+	console.log(data);
+});
+
 router.get("/", (req, res) => {
 	res.json({
 		items,
--- server/src/db.js
+++ server/src/db.js
@@ -6,8 +6,13 @@ const db = new sqlite3.Database("./db/todo.db", (err) => {
 	if (err) throw err;
 });
 // create table
-sql = `CREATE TABLE todo(uuid BLOB PRIMARY KEY, label TEXT, done INTEGER)`;
-db.run(sql);
+// sql = `CREATE TABLE todo(uuid BLOB PRIMARY KEY, label TEXT, done INTEGER)`;
+// db.run(sql);
+db.run("INSERT INTO todo(uuid, label, done) VALUES(?,?,?)", [
+	"abcdef",
+	"test",
+	0,
+]);
 
 db.close((err) => {
 	if (err) throw err;
